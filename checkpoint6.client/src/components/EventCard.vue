28d43324e1cf74f3e1baa24ef29f8b3689ee4844
LiamSmith1992 <liamj.smith@hotmail.com>
2022-12-16 14:08:52 -0700

    finito

--- checkpoint6.client/src/components/EventCard.vue
+++ checkpoint6.client/src/components/EventCard.vue
@@ -1,10 +1,12 @@
 <template>
   <router-link :to="{ name: 'Event', params: { eventId: event.id } }">
-    <div>
+    <div class="card">
       <div v-if="event.isCanceled" class="bg-danger ">Canceled 😭</div>
       <div class="text-danger" v-if="event.capacity == 0">Sold Out</div>
       <img :src="event.coverImg" alt="" class="img-fluid cover-img rounded">
-      {{ event.name }}
+      <h4 class="text-center">
+        {{ event.name }}
+      </h4>
     </div>
   </router-link>
 </template>
